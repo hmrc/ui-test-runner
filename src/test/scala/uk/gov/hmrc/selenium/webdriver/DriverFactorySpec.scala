fb993d281804f75043420aef262d4bb14fcb5415
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2023-08-08 14:19:06 +0100

    PLATUI-BAU: Remove custom browser options

--- src/test/scala/uk/gov/hmrc/selenium/webdriver/DriverFactorySpec.scala
+++ src/test/scala/uk/gov/hmrc/selenium/webdriver/DriverFactorySpec.scala
@@ -33,7 +33,7 @@ class DriverFactorySpec extends AnyWordSpec with Matchers {
   "DriverFactory" should {
 
     "return default Chrome options" in new Setup {
-      val options: ChromeOptions   = driverFactory.chromeOptions(None)
+      val options: ChromeOptions   = driverFactory.chromeOptions()
       val encodedExtension: String =
         Source.fromResource("extensions/chrome/accessibility-assessment").getLines().mkString
 
@@ -44,26 +44,8 @@ class DriverFactorySpec extends AnyWordSpec with Matchers {
         .toString                        shouldBe s"{args=[--remote-allow-origins=*], extensions=[$encodedExtension]}"
     }
 
-    "return merged Chrome options" in new Setup {
-      val capabilities = new ChromeOptions
-
-      capabilities.setAcceptInsecureCerts(true)
-      capabilities.addArguments("--headless=new")
-
-      val options: ChromeOptions   = driverFactory.chromeOptions(Some(capabilities))
-      val encodedExtension: String =
-        Source.fromResource("extensions/chrome/accessibility-assessment").getLines().mkString
-
-      options.asMap().get("acceptInsecureCerts") shouldBe true
-      options.asMap().get("browserName")         shouldBe "chrome"
-      options
-        .asMap()
-        .get("goog:chromeOptions")
-        .toString                                shouldBe s"{args=[--remote-allow-origins=*, --headless=new], extensions=[$encodedExtension]}"
-    }
-
     "return default Edge options" in new Setup {
-      val options: EdgeOptions     = driverFactory.edgeOptions(None)
+      val options: EdgeOptions     = driverFactory.edgeOptions()
       val encodedExtension: String = Source.fromResource("extensions/edge/accessibility-assessment").getLines().mkString
 
       options.asMap().get("browserName") shouldBe "MicrosoftEdge"
@@ -73,41 +55,11 @@ class DriverFactorySpec extends AnyWordSpec with Matchers {
         .toString                        shouldBe s"{args=[--remote-allow-origins=*], extensions=[$encodedExtension]}"
     }
 
-    "return merged Edge options" in new Setup {
-      val capabilities = new EdgeOptions
-
-      capabilities.setAcceptInsecureCerts(true)
-      capabilities.addArguments("--headless=new")
-
-      val options: EdgeOptions     = driverFactory.edgeOptions(Some(capabilities))
-      val encodedExtension: String = Source.fromResource("extensions/edge/accessibility-assessment").getLines().mkString
-
-      options.asMap().get("acceptInsecureCerts") shouldBe true
-      options.asMap().get("browserName")         shouldBe "MicrosoftEdge"
-      options
-        .asMap()
-        .get("ms:edgeOptions")
-        .toString                                shouldBe s"{args=[--remote-allow-origins=*, --headless=new], extensions=[$encodedExtension]}"
-    }
-
     "return default Firefox options" in new Setup {
-      val options: FirefoxOptions = driverFactory.firefoxOptions(None)
+      val options: FirefoxOptions = driverFactory.firefoxOptions()
 
       options.asMap().get("browserName")                 shouldBe "firefox"
       options.asMap().get("moz:firefoxOptions").toString shouldBe "{}"
-    }
-
-    "return merged Firefox options" in new Setup {
-      val capabilities = new FirefoxOptions
-
-      capabilities.setAcceptInsecureCerts(true)
-      capabilities.addArguments("-headless")
-
-      val options: FirefoxOptions = driverFactory.firefoxOptions(Some(capabilities))
-
-      options.asMap().get("acceptInsecureCerts")         shouldBe true
-      options.asMap().get("browserName")                 shouldBe "firefox"
-      options.asMap().get("moz:firefoxOptions").toString shouldBe "{args=[-headless]}"
     }
 
   }
