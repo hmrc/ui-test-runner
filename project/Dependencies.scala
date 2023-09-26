8d29ce34074ba18cef177852d05cf3ed3cd2dfec
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2023-09-26 13:10:22 +0100

    PLATUI-BAU: Update project dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,8 @@ object Dependencies {
     "com.typesafe.play"          %% "play-json"     % "2.9.4",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
     "com.vladsch.flexmark"        % "flexmark-all"  % "0.62.2",
-    "org.scalatest"              %% "scalatest"     % "3.2.16",
-    "org.seleniumhq.selenium"     % "selenium-java" % "4.11.0",
+    "org.scalatest"              %% "scalatest"     % "3.2.17",
+    "org.seleniumhq.selenium"     % "selenium-java" % "4.13.0",
     "org.slf4j"                   % "slf4j-simple"  % "1.7.36"
   )
 
