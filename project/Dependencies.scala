22d2893cc4a1aa903947187f81e6d17f4b06613a
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-02-23 11:09:23 +0000

    PLATUI-BAU: Update flexmark version to 0.64.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "com.typesafe"                % "config"        % "1.4.3",
     "com.typesafe.play"          %% "play-json"     % "2.10.4",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
-    "com.vladsch.flexmark"        % "flexmark-all"  % "0.62.2",
+    "com.vladsch.flexmark"        % "flexmark-all"  % "0.64.8",
     "org.scalatest"              %% "scalatest"     % "3.2.18",
     "org.seleniumhq.selenium"     % "selenium-java" % "4.16.1",
     "org.slf4j"                   % "slf4j-simple"  % "1.7.36"
