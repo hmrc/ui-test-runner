a583d1b1a75111932e153d977d55fb4d21c4dd45
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-02-23 11:07:37 +0000

    PLATUI-BAU: Update play-json version to 2.10.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "com.typesafe"                % "config"        % "1.4.3",
-    "com.typesafe.play"          %% "play-json"     % "2.9.4",
+    "com.typesafe.play"          %% "play-json"     % "2.10.4",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
     "com.vladsch.flexmark"        % "flexmark-all"  % "0.62.2",
     "org.scalatest"              %% "scalatest"     % "3.2.17",
