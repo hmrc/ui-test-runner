f684155b4108a8de7bb7dfd3b163ba1341dde677
wayne ellis <9093361+funkmaster20th@users.noreply.github.com>
2024-11-20 09:04:33 +0000

    PLATUI-selbump bumped the selenium version to the latest that still supports 126

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,6 +8,6 @@ object Dependencies {
     "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5",
     "com.vladsch.flexmark"        % "flexmark-all"    % "0.64.8",
     "org.scalatest"              %% "scalatest"       % "3.2.19",
-    "org.seleniumhq.selenium"     % "selenium-java"   % "4.22.0"
+    "org.seleniumhq.selenium"     % "selenium-java"   % "4.24.0"
   )
 }
